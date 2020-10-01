3784a69e18fc7f4481f1e0e68c8ce613a27ec87b
Hikari-hong <61600625+Hikari-hong@users.noreply.github.com>
2020-10-02 04:54:41 +0900

    8강 끝

---
+++ Coding_8/src/a/Test.java
@@ -0,0 +1,12 @@
+package a;
+
+public class Test {
+
+    // default, public, private, protected
+    protected String a = "타카노";
+
+    public static void main(String[] args) {
+        // TODO: 2020/10/02
+
+    }
+}
