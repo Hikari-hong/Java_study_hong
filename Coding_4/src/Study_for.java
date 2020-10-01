8e6789418de5e7bcb8ad006241e0d36585b31634
Hikari-hong <61600625+Hikari-hong@users.noreply.github.com>
2020-10-02 03:16:08 +0900

    4강

--- Coding_4/src/Study_for.java
+++ Coding_4/src/Study_for.java
@@ -3,10 +3,16 @@ public class Study_for {
     public static void main(String[] args) {
         // TODO: 2020/10/02
 
+        String str[] = {"하나","둘","셋"};
+
         for (int i=0; i<3; i++) { // 0부터 시작해서 ++로 1씩 증가, 3보다 작을때까지 true.
              System.out.println("타카노");
         }
         System.out.println("잘생겼다.");
+
+        for (int i=0; i<str.length; i++) {
+            System.out.println("인정?");
+        }
 
     }
 
