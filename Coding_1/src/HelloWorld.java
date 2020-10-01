d6e8c9b3766d62eaad24d557e9101d8cbad2314c
Hikari-hong <61600625+Hikari-hong@users.noreply.github.com>
2020-10-02 02:03:03 +0900

    제1강 끝.

---
+++ Coding_1/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) { // 메인 함수이며, 우선으로 실행되는 메소드.
+        // TODO Auto-generated method stub
+        System.out.println("타카노가 자바 공부를 한다."); // 로그를 출력.
+        System.out.println("Android Java강의.");
+        System.out.print("타카노가 자바 공부를 한다."); // 로그를 출력. println은 줄바꿈이 존재. print는 줄바꿈 없.
+        System.out.print("Android Java강의.");
+    }
+
+}
