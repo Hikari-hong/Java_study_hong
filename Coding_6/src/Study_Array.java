7b0b0b7059f77dfff4c3cd39adb27fbbcb5a1d08
Hikari-hong <61600625+Hikari-hong@users.noreply.github.com>
2020-10-02 03:58:26 +0900

    6강 끝

--- Coding_6/src/Study_Array.java
+++ Coding_6/src/Study_Array.java
@@ -1,6 +1,31 @@
 public class Study_Array {
     public static void main(String[] args) {
         // TODO: 2020/10/02
+        String a = "a";
+        int b = 1;
+
+        int arrayInt[] = new int[10]; // 0부터 9까지의 수를 담고 있음.
+        for (int i = 0; i<arrayInt.length; i++) {
+            System.out.println(i);
+        }
+
+
+        int arrayInt2[] = {1,3,5,7,9};
+        for (int i = 0; i<arrayInt2.length; i++) {
+            System.out.println(i);
+        }
+
+
+        int arrayInt3[] = {1,3,5,7,9};
+        for (int i = 0; i<arrayInt3.length; i++) {
+            System.out.println(arrayInt3[i]);
+        }
+
+
+        String arraystr[] = {"a","b","c","d","e"};
+        for (int i = 0; i<arraystr.length; i++) {
+            System.out.println(arraystr[i]);
+        }
 
     }
 }
