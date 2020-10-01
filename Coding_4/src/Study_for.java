a05d1148df88b2406957afee4853bdd14fbec762
Hikari-hong <61600625+Hikari-hong@users.noreply.github.com>
2020-10-02 03:13:11 +0900

    4강

--- Coding_4/src/Study_for.java
+++ Coding_4/src/Study_for.java
@@ -1,6 +1,13 @@
 public class Study_for {
-}
 
     public static void main(String[] args) {
-        // TODO: 2020/10/02  
+        // TODO: 2020/10/02
+
+        for (int i=0; i<3; i++) { // 0부터 시작해서 ++로 1씩 증가, 3보다 작을때까지 true.
+             System.out.println("타카노");
+        }
+        System.out.println("잘생겼다.");
+
     }
+
+}
